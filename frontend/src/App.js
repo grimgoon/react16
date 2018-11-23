754b46e81c3f603ca1528ec6a9e020ce09944851
sls_grim <alexanderh@stunlockstudios.com>
2018-11-23 11:45:34 +0100

    Lecture 50 of 433

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       {name : 'Alexander', age: '24'},
       {name : 'Stephanie', age: '25'},
     ],
-    testState: 'Some other value'
+    testState: 'Some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -34,6 +35,11 @@ class App extends Component {
       } )
   }
 
+  toggglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons : !doesShow});
+  }
+
   render() {
 
     const buttonStyle = {
@@ -49,19 +55,22 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button style={buttonStyle} onClick={() => this.switchNameHandler('TestName!')}>Switch Name</button>
-
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
-        <Person 
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-        <Person 
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}/>
+        <button style={buttonStyle} onClick={this.toggglePersonsHandler}>Toggle Persons</button>
+        
+        { this.state.showPersons === true ? <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+          <Person 
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}/>
+        </div> : null
+        }
 
       </div>
     );
