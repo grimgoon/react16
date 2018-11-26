dd70cafaae901685b51301fbb4bad7615ebac883
sls_grim <alexander@hermansson.us>
2018-11-26 14:03:24 +0100

    Lecture 84 of 433

--- frontend/src/components/Persons/Persons.js
+++ frontend/src/components/Persons/Persons.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person.js'
 
 // Can directly return by doing like below:
-const persons = (props) =>  props.persons.map( ( person, index ) => {
-        return <Person
-            click={() => props.clicked( index )}
-            name={person.name}
-            key={person.id}
-            age={person.age}
-            changed={( event ) => props.changed( event, person.id )}/>
-      } );
+class Persons extends Component{
+    render () {
+        return this.props.persons.map( ( person, index ) => {
+            return <Person
+                click={() => this.props.clicked( index )}
+                name={person.name}
+                key={person.id}
+                age={person.age}
+                changed={( event ) => this.props.changed( event, person.id )}/>
+          });
+    }
+ 
+}
 
-export default persons;
+export default Persons;
