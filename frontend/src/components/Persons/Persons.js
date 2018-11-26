6ca2d0843b89ebe994ab5ccdf841e4c59b36f21d
sls_grim <alexander@hermansson.us>
2018-11-26 16:29:44 +0100

    Lecture 88 of 433

--- frontend/src/components/Persons/Persons.js
+++ frontend/src/components/Persons/Persons.js
@@ -17,8 +17,27 @@ class Persons extends Component{
         console.log('[Persons.js] Did Mount');
       }
 
+      componentWillReceiveProps(nextProps)
+      {
+          console.log('[UPDATE Persons.js] Inside componentWillReceiveProps',nextProps)          
+      }
+
+      shouldComponentUpdate(nextProps, nextState)
+      {
+          console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+          return nextProps.persons !== this.props.persons;
+      }
+
+      componentWillUpdate(nextProps, nextState) {
+        console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
+      }
+
+      componentDidUpdate() {
+        console.log('[UPDATE Persons.js] Inside componentDidUpdate');
+      }
+
     render () {
-        console.log('[Persons.js] Rendert');
+        console.log('[Persons.js] Render');
         return this.props.persons.map( ( person, index ) => {
             return <Person
                 click={() => this.props.clicked( index )}
