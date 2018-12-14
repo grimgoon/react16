fd6a74960f2a27440be37edad43f9275552572dd
sls_grim <alexander@hermansson.us>
2018-12-14 14:13:05 +0100

    Lecture 137 of 433

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <>
         <Toolbar />
+        <SideDrawer/>
         <main className={style.Content}>
             {props.children}
         </main>
