c55fe45b0147c2a2bcafa8c853f7bb299e829cfa
sls_grim <alexander@hermansson.us>
2018-12-18 13:54:22 +0100

    Lecture 172 of 433

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -107,7 +108,27 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+
+        const order = {
+            ingredients : this.state.ingredients,
+            price : this.state.totalPrice,
+            customer : {
+                name : 'Alexander Hermansson',
+                address : {
+                    street : 'Test Street 1',
+                    zipCode : '5140',
+                    country : 'Sweden',
+                },
+                email : 'test@test.com',  
+            },
+            deliveryMethod : 'fastest'
+        }
+
+        axios.post('/orders.json', order)
+            .then(respone => console.log(respone))
+            .catch(error => console.log(error));
+
     }
 
     render(){
