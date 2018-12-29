a423f4272412643f43ca37b48a78b043de1a0c78
grimgoon <alexander@hermansson.us>
2018-12-29 16:42:43 +0100

    Lecture 206 of 456

--- http--01/src/containers/Blog/Blog.js
+++ http--01/src/containers/Blog/Blog.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
-
+ 
 import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost'); 
+});
 
 class Blog extends Component {
 
     state = {
-        auth : false,
+        auth : true,
     }
 
     render () {
@@ -31,7 +36,7 @@ class Blog extends Component {
                 
                 {/* <Route path="/" exact render={() => <h1>Home</h1>}/> */}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" exact component={NewPost}/> : null }
+                    {this.state.auth ? <Route path="/new-post" exact component={AsyncNewPost}/> : null }
                     <Route path="/posts" component={Posts}/>
                     <Route path="/" exact component={Posts}/>
                     <Route render={() => <h1>404 not found</h1>}/>
