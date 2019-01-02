24ecd0171c897ca6790bf08019cd63291ae08300
grimgoon <alexander@hermansson.us>
2019-01-02 13:22:38 +0100

    Lecture 214 of 456

--- burger/src/components/Burger/Burger.js
+++ burger/src/components/Burger/Burger.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import {withRouter} from 'react-router-dom';
 
 const burger = (props) => {
 
@@ -27,4 +28,4 @@ const burger = (props) => {
     );
 }
 
-export default burger;
+export default withRouter(burger);
--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,6 +34,8 @@ class BurgerBuilder extends Component {
             }).catch(error => {
                 this.setState({error : true});
             });
+
+        console.log(this.props);
     }
 
     updatePurchaseState = (ingredients) => {
@@ -117,32 +119,34 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
+        // // alert('You continue!');
 
-        this.setState({loading : true});
+        // this.setState({loading : true});
 
-        const order = {
-            ingredients : this.state.ingredients,
-            price : this.state.totalPrice,
-            customer : {
-                name : 'Alexander Hermansson',
-                address : {
-                    street : 'Test Street 1',
-                    zipCode : '5140',
-                    country : 'Sweden',
-                },
-                email : 'test@test.com',  
-            },
-            deliveryMethod : 'fastest'
-        }
+        // const order = {
+        //     ingredients : this.state.ingredients,
+        //     price : this.state.totalPrice,
+        //     customer : {
+        //         name : 'Alexander Hermansson',
+        //         address : {
+        //             street : 'Test Street 1',
+        //             zipCode : '5140',
+        //             country : 'Sweden',
+        //         },
+        //         email : 'test@test.com',  
+        //     },
+        //     deliveryMethod : 'fastest'
+        // }
 
-        axios.post('/orders.json', order)
-            .then(respone => {
-                this.setState({loading : false, purchasing : false});
-            })
-            .catch(error => {
-                this.setState({loading : false, purchasing : false})
-            });
+        // axios.post('/orders.json', order)
+        //     .then(respone => {
+        //         this.setState({loading : false, purchasing : false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading : false, purchasing : false})
+        //     });
+
+        this.props.history.push('/checkout');
 
     }
 
