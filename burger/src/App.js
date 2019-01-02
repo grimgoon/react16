762a56f27b25236571ea15b7752fac9ec4823793
grimgoon <alexander@hermansson.us>
2019-01-02 13:12:43 +0100

    Lecture 212 of 456

--- burger/src/App.js
+++ burger/src/App.js
@@ -3,6 +3,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 
+
 class App extends Component {
   render() {
     return (
