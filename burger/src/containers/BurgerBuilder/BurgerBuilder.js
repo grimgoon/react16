83bb165a4d908fc9b6f6653b3c1df2e33527c709
grimgoon <alexander@hermansson.us>
2019-01-04 11:45:32 +0100

    Lecture 218 of 456

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -119,38 +119,14 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // // alert('You continue!');
-
-        // this.setState({loading : true});
-
-        // const order = {
-        //     ingredients : this.state.ingredients,
-        //     price : this.state.totalPrice,
-        //     customer : {
-        //         name : 'Alexander Hermansson',
-        //         address : {
-        //             street : 'Test Street 1',
-        //             zipCode : '5140',
-        //             country : 'Sweden',
-        //         },
-        //         email : 'test@test.com',  
-        //     },
-        //     deliveryMethod : 'fastest'
-        // }
-
-        // axios.post('/orders.json', order)
-        //     .then(respone => {
-        //         this.setState({loading : false, purchasing : false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading : false, purchasing : false})
-        //     });
-
-
+        
         const queryParams = [];
         for(let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
+        }  
+    
+        queryParams.push('price=' + this.state.totalPrice);
+
         const queryString = queryParams.join('&');
 
         this.props.history.push({
--- burger/src/containers/Checkout/ContactData/ContactData.js
+++ burger/src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import styles from './ContactData.module.css';
+import axios from '../../../axios-orders';
+
+
 class ContactData extends Component {
 
     state = {
@@ -9,20 +13,59 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: '',
+        },
+        loading: false,
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+
+        this.setState({loading : true});
+
+        const order = {
+            ingredients : this.state.ingredients,
+            price : this.props.price,
+            customer : {
+                name : 'Alexander Hermansson',
+                address : {
+                    street : 'Test Street 1',
+                    zipCode : '5140',
+                    country : 'Sweden',
+                },
+                email : 'test@test.com',  
+            },
+            deliveryMethod : 'fastest'
         }
+
+        axios.post('/orders.json', order)
+            .then(respone => {
+                this.setState({loading : false});
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({loading : false})
+            });
     }
 
     render() {
+
+        let form = (
+        <form>
+            <input className={styles.Input} type="text" name="name" placeholder="Your Name"/>
+            <input className={styles.Input} type="text" name="email" placeholder="Your Email"/>
+            <input className={styles.Input} type="text" name="street" placeholder="Your Street"/>
+            <input className={styles.Input} type="text" name="postal" placeholder="Postcode"/>
+            <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
+        </form>);
+
+        if(this.state.loading) {
+            form = <Spinner />
+        }
+
         return(
             <div className={styles.ContactData}>
                 <h4>Enter your Contact Data</h4>
-                <form>
-                    <input className={styles.Input} type="text" name="name" placeholder="Your Name"/>
-                    <input className={styles.Input} type="text" name="email" placeholder="Your Email"/>
-                    <input className={styles.Input} type="text" name="street" placeholder="Your Street"/>
-                    <input className={styles.Input} type="text" name="postal" placeholder="Postcode"/>
-                    <Button btnType="Success">ORDER</Button>
-                </form>
+                {form}
             </div>
         )
     }
