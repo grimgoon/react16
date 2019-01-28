1a0e2dcb57d66df2d8f979d3cbfe9e9d882dcf63
sls_grim <alexander@hermansson.us>
2019-01-28 16:49:03 +0100

    Lecture 234 of 456

--- burger/src/containers/Checkout/ContactData/ContactData.js
+++ burger/src/containers/Checkout/ContactData/ContactData.js
@@ -43,6 +43,8 @@ class ContactData extends Component {
                 value : '',
                 validation : {
                     required : true,
+                    minLength: 5,
+                    maxLength: 5,
                 },
                 valid : false,
             },
@@ -119,10 +121,18 @@ class ContactData extends Component {
 
     checkValidity = (value, rules) => {
 
-        let isValid = false;
+        let isValid = true;
 
         if(rules.required) {
-            isValid = value.trim() !== '';
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if(rules.maxLength) {
+            isValid = value.length <= rules.minLength && isValid;
         }
 
         return isValid;
