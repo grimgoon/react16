6ab22a476b0ecec27ec60424b57700467922d1f4
grimgoon <alexander@hermansson.us>
2019-02-04 20:49:37 +0100

    Assignment 4

--- redux--01/src/store/reducer.js
+++ redux--01/src/store/reducer.js
@@ -10,6 +10,24 @@ const reducer = (state = initialState,action) => {
         }
     }
 
+    if(action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - 5
+        }
+    }
+
+    if(action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    if(action.type === 'ADD') {
+        return {
+            counter: state.counter + 5
+        }
+    }
+
     return state;
 }
 
