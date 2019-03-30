787cca90d3dd88cbf5f2f8db525dc4625ff0a4ca
grimgoon <alexander@hermansson.us>
2019-03-30 15:20:41 +0100

    Assignment 5

--- redux--01/src/containers/Counter/Counter.js
+++ redux--01/src/containers/Counter/Counter.js
@@ -14,19 +14,11 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
-<<<<<<< HEAD
-                <hr/>
-                <button onClick={this.props.onStoreResult}>Store Result</button>
-                <ul>
-                    {this.props.storedResults.map(result => (
-                        <li key={result.id} onClick={this.props.onDeleteResult}>{result.value}</li>
-=======
                 <hr />
                 <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResult => (
                         <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
->>>>>>> 2ce9f100650678804f83dc02502ac1f3da56ce31
                     ))}
                 </ul>
             </div>
@@ -36,22 +28,6 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-<<<<<<< HEAD
-        ctr: state.counter,
-        storedResults: state.results
-    };
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-      onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-      onSubtractCounter: () => dispatch({type: 'SUBTRACT', payload: 15}),
-      onAddCounter: () => dispatch({type: 'ADD', payload: 10}),
-      onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-      onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
-      deleteResult: () => dispatch({type: 'DELETE_RESULT'}),
-  }  
-=======
 
         ctr: state.ctr.counter,
         storedResults: state.res.results
@@ -67,7 +43,6 @@ const mapDispatchToProps = dispatch => {
         onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
         onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
     }
->>>>>>> 2ce9f100650678804f83dc02502ac1f3da56ce31
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
