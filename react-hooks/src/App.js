832ffe14a9f55ad75e2e5d0e8dd96be9081cb872
grimgoon <alexander@hermansson.us>
2019-06-23 21:35:05 +0200

    Lecture 468

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import Header from './components/Header';
 import Todo from './components/Todo';
+import Auth from './components/Auth';
 
-function App() {
+function App(props) {
+
+  const [page, setPage] = useState('auth');
+
+  const switchPage = (pageName) => {
+    setPage(pageName);
+  }
+
   return (
     <div className="App">
-      <Todo/>
+      <Header 
+        onLoadTodos={() => switchPage('todos')} 
+        onLoadAuth={switchPage.bind(this, 'auth')}/>
+      <hr/>
+      {page === 'auth' ? <Auth/> : <Todo/>}
+      
+      
     </div>
   );
 }
