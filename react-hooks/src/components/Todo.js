b29485d620cb381ad84f2e8da64c69ef419dafc0
grimgoon <alexander@hermansson.us>
2019-06-23 11:19:30 +0200

    Lecture 461

--- react-hooks/src/components/Todo.js
+++ react-hooks/src/components/Todo.js
@@ -2,16 +2,21 @@ import React, {useState} from 'react';
 
 const Todo = props => {
     const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([]);
 
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value);
     }
 
+    const todoAddHandler = () => {
+        setTodoList(todoList.concat(todoName));
+    }
+
     return  <>
         <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoName}/>
-        <button type="button">Add</button>
+        <button onClick={todoAddHandler} type="button">Add</button>
         <ul>
-
+            {todoList.map((todo,i) => <li key={i}>{todo}</li>)}
         </ul>
     </>
 }
