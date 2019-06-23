2ce7fd476963ceaae8ee18ac6ea443243ac79533
grimgoon <alexander@hermansson.us>
2019-06-23 14:36:01 +0200

    Lecure 463

--- react-hooks/src/components/Todo.js
+++ react-hooks/src/components/Todo.js
@@ -1,24 +1,22 @@
 import React, {useState} from 'react';
 
 const Todo = props => {
-    // const [todoName, setTodoName] = useState('');
-    // const [todoList, setTodoList] = useState([]);
-    
-    const [todoState, setTodoState] = useState({userInput: '', todoList: []})
+    const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([]);
 
     const inputChangeHandler = (event) => {
-        setTodoState({userInput: event.target.value, todoList: todoState.todoList});
+        setTodoName(event.target.value);
     }
 
     const todoAddHandler = () => {
-        setTodoState({userInput: todoState.userInput, todoList: todoState.todoList.concat(todoState.userInput)});
+        setTodoList(todoList.concat(todoName));
     }
 
     return  <>
-        <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoState.userInput}/>
+        <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoName}/>
         <button onClick={todoAddHandler} type="button">Add</button>
         <ul>
-            {todoState.todoList.map((todo,i) => <li key={i}>{todo}</li>)}
+            {todoList.map((todo,i) => <li key={i}>{todo}</li>)}
         </ul>
     </>
 }
