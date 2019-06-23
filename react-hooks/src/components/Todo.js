299ee08d6b2e0392a44e0bbb06cb8af537a1a9dc
grimgoon <alexander@hermansson.us>
2019-06-23 14:54:48 +0200

    Lecture 465

--- react-hooks/src/components/Todo.js
+++ react-hooks/src/components/Todo.js
@@ -1,9 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 
 const Todo = props => {
     const [todoName, setTodoName] = useState('');
     const [todoList, setTodoList] = useState([]);
+
+
+    
+    useEffect(() => {
+        axios.get('https://test-help-ooqq.firebaseio.com/todos.json')
+            .then((res) => {
+                const todoData = res.data;
+                const todos = []
+                for(const key in todoData) {
+                    todos.push({id: key, name: todoData[key].name});
+                }
+                setTodoList(todos);
+            });
+    });
 
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value);
@@ -24,7 +38,7 @@ const Todo = props => {
         <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoName}/>
         <button onClick={todoAddHandler} type="button">Add</button>
         <ul>
-            {todoList.map((todo,i) => <li key={i}>{todo}</li>)}
+            {todoList.map((todo,i) => <li key={i}>{todo.name}</li>)}
         </ul>
     </>
 }
