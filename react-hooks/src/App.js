4178ce4187eb3c7a68b6b1f6659d259efa93ecf3
grimgoon <alexander@hermansson.us>
2019-07-03 01:52:05 +0200

    Lecture 469

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,26 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import Header from './components/Header';
-import Todo from './components/Todo';
-import Auth from './components/Auth';
+import Header from "./components/Header";
+import Todo from "./components/Todo";
+import Auth from "./components/Auth";
+import AuthContext from "./auth-context";
 
 function App(props) {
-
-  const [page, setPage] = useState('auth');
-
-  const switchPage = (pageName) => {
+  const [page, setPage] = useState("auth");
+  const [authStatus, setAuthStatus] = useState(false);
+  const switchPage = pageName => {
     setPage(pageName);
+  };
+
+  const login = () => {
+    setAuthStatus(true);
   }
 
   return (
     <div className="App">
-      <Header 
-        onLoadTodos={() => switchPage('todos')} 
-        onLoadAuth={switchPage.bind(this, 'auth')}/>
-      <hr/>
-      {page === 'auth' ? <Auth/> : <Todo/>}
-      
-      
+      <AuthContext.Provider value={{status: authStatus, login}}>
+        <Header
+          onLoadTodos={() => switchPage("todos")}
+          onLoadAuth={switchPage.bind(this, "auth")}
+        />
+        <hr />
+        {page === "auth" ? <Auth /> : <Todo />}
+      </AuthContext.Provider>
     </div>
   );
 }
