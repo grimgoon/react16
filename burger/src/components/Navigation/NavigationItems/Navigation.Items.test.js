bdefe16040fa1d88af7a2177f2b9b657b100ac1c
grimgoon <alexander@hermansson.us>
2019-08-12 14:19:09 +0200

    Lecture 384

--- burger/src/components/Navigation/NavigationItems/Navigation.Items.test.js
+++ burger/src/components/Navigation/NavigationItems/Navigation.Items.test.js
@@ -7,8 +7,17 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('NavigationItems', () => {
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems/>);
+    }) 
+
     it('should render 2 NavigationItem elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems/>)
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+    it('should render 3 NavigationItem elements IF authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 });
