41bde6a8bef0b314a00899f0697b3b329ff71caa
Alexander Hermansson <alexander@hermansson.us>
2019-10-15 15:12:18 +0200

    Lecture 421

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Link from "next/Link";
+import Router from "next/router";
 
 const IndexPage = () => {
   return (
     <div>
       <h1>The Main Page</h1>
+      <Link href="/">
+        <a>Meep</a>
+      </Link>
+      <button onClick={() => Router.push("/auth")}> Button</button>
     </div>
   );
 };
