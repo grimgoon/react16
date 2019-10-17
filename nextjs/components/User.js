b9b3f51b4f08919e1f004cf021e0bea191955410
Alexander Hermansson <alexander@hermansson.us>
2019-10-17 08:46:36 +0200

    Lecture 423

--- nextjs/components/User.js
+++ nextjs/components/User.js
@@ -4,6 +4,13 @@ const User = props => (
   <div>
     <h1>{props.name}</h1>
     <p>Age: {props.age}</p>
+    <style jsx>{`
+      div {
+        border: 2px solid #eee;
+        box-shadow: 0 2px 3px #ccc;
+        text-align: center;
+      }
+    `}</style>
   </div>
 );
 
