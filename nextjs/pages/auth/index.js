3bd25fce4c491603504bbb73bc0c16589a16552a
Alexander Hermansson <alexander@hermansson.us>
2019-10-17 08:43:41 +0200

    Lecture 422

--- nextjs/pages/auth/index.js
+++ nextjs/pages/auth/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Link from "next/link";
+
+import User from "../../components/User";
 
 const IndexPage = () => {
   return (
     <div>
       <h1>The Auth Index Page</h1>
+      <User name="Alex" age={28} />
     </div>
   );
 };
