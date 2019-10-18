9ebde38a77a8f1a2915acd1b58f501201e15f47f
Alexander Hermansson <alexander@hermansson.us>
2019-10-18 09:03:41 +0200

    Lecture 410

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
   entry: "./src/index.js",
@@ -15,7 +16,36 @@ module.exports = {
     rules: [
       {
         test: /\(.js|jsx)$/,
-        loader: "",
+        // Short version of use
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: "[name]__[local]__[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: [
+                autoprefixer({
+                  browsers: ["> 1%", "last 2 versions"]
+                })
+              ]
+            }
+          }
+        ],
         exclude: /node_modules/
       }
     ]
