9b4aee76f73f736316deb54ccd42c23ef0a7d690
grimgoon <alexander@hermansson.us>
2019-11-17 20:51:48 +0100

    mmh

--- react-hooks/src/components/Auth.js
+++ react-hooks/src/components/Auth.js
@@ -1,16 +1,9 @@
-import React,{useContext}  from 'react';
+import React, { useContext } from "react";
 import AuthContext from "../auth-context";
 
-<<<<<<< HEAD
-const Auth = props => (
-    <button>Log in!</button>
-    
-);
-=======
 const Auth = props => {
-    const auth = useContext(AuthContext);
-   return <button onClick={auth.login}>Log in!</button>
+  const auth = useContext(AuthContext);
+  return <button onClick={auth.login}>Log in!</button>;
 };
->>>>>>> aa4eb65643edb95b2d883b97d6534ee7bfe818bd
 
 export default Auth;
